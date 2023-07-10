1c01cb34f7481322af4254d6b8967e2c779fe0a1
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-07-10 16:48:34 +0200

    update comment's score functionality implemented

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function generateCard(commentData, typeOfCard) {
+  console.log(typeOfCard);
   return `
     <div class="${typeOfCard}">
         <div class="vote-section">
         <div class="btn-group">
-            <button><img src="./images/icon-plus.svg" alt="icon_plus"></button>
-            <p>${commentData.score}</p>
-            <button><img src="./images/icon-minus.svg" alt="icon_minus"></button>
+            <button onClick="updateScore(${commentData.id}, 'increase', '${typeOfCard}')"><img src="./images/icon-plus.svg" alt="icon_plus"></button>
+            <p id="score_${typeOfCard}_${commentData.id}">${commentData.score}</p>
+            <button onClick="updateScore(${commentData.id}, 'decrease', '${typeOfCard}')"><img src="./images/icon-minus.svg" alt="icon_minus"></button>
             </div>
         </div>
         <div class="card-main-section">
@@ -14,7 +15,6 @@ function generateCard(commentData, typeOfCard) {
                     <li><img class="user-image" src="${commentData.user.image.webp}" alt="" srcset=""></li>
                     <li class="user-name">${commentData.user.username}</li>
                     <li>${commentData.createdAt}</li>
-                    
                 </ul>
                 <ul>
               <li><img class="reply-icon" src="./images/icon-reply.svg" alt="" srcset=""> Reply</li>
@@ -43,7 +43,7 @@ function generateLayout() {
           repliesDiv.classList.add("replies-box");
           comment.replies.forEach((reply) => {
             commentDiv.insertAdjacentElement("beforeend", repliesDiv);
-            repliesDiv.innerHTML += generateCard(reply, "comment-card");
+            repliesDiv.innerHTML += generateCard(reply, "reply-card");
           });
         }
         commentsBoxEl.insertAdjacentElement("beforeend", commentDiv);
@@ -58,4 +58,68 @@ async function getCommentInformations() {
   return fetch("./data.json");
 }
 
+function onInit() {
+  getCommentInformations().then((res) =>
+    res.json().then((data) => {
+      localStorage.setItem("comments", JSON.stringify(data.comments));
+      localStorage.setItem("currentUser", JSON.stringify(data.currentUser));
+    })
+  );
+}
+
+function updateScore(commentId, type, typeOfCard) {
+  const comments = getComments();
+  let data;
+  if (typeOfCard === "comment-card") {
+    data = findCommentById(comments, commentId);
+  } else {
+    data = findReplyById(comments, commentId);
+  }
+  if (data) {
+    if (type === "increase") {
+      data.score++;
+    } else if (type === "decrease") {
+      if (data.score > 0) data.score--;
+    }
+    refreshHtmlScore(data.score, commentId, typeOfCard);
+    setComments(comments);
+  }
+}
+
+function refreshHtmlScore(score, id, typeOfCard) {
+  const scoreEl = document.getElementById(`score_${typeOfCard}_${id}`);
+  if (scoreEl) scoreEl.textContent = score;
+}
+
+//Comment functions
+function getComments() {
+  return JSON.parse(localStorage.getItem("comments"));
+}
+function setComments(comments) {
+  return localStorage.setItem("comments", JSON.stringify(comments));
+}
+
+function findCommentById(comments, id) {
+  return comments.find((comment) => comment.id === id);
+}
+
+//Reply functions
+
+function addReply(id, reply) {
+  const comment = getCommentById(id);
+  comment.replies.push(reply);
+}
+function findReplyById(comments, id) {
+  let reply;
+  comments.forEach((comment) => {
+    if (comment.replies.length > 0) {
+      reply = comment.replies.find((reply) => reply.id === id);
+    }
+  });
+  return reply;
+}
+
+function saveReplyToLocalStorage(id, reply) {}
+
 generateLayout();
+onInit();
